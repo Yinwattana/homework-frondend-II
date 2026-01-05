92d0842a3a60e2769e1e4a5354a93a0f88abf8d1
Yinwattana <yinwattana2024@gmail.com>
2026-01-05 15:14:22 +0700

    update mt

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
+      <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 mt-6">
         <PostCardList />
       </section>
     </main>
